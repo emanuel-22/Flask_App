0cf75691a6e6fb2ee0ecffbcc0f41356a01ca503
Emanuel Barboza <emanuel.barboza@nuntius.tech>
2024-12-26 00:35:10 -0300

    cambios de ema

--- main.py
+++ main.py
@@ -20,16 +20,20 @@ Los motodos son:
 # Esto es para pasar parametros
 @app.route('/users/<user_id>')
 def get_user(user_id):
-    user = {"id": user_id, "name": "test", "telefono": "9999-222-21"}
-    # /users/2654?query=query_test
-    # query es un parametro al igual que user_id
-    query= request.args.get('query')
-    if query:
-        user["query"]=query 
-    return jsonify(user), 200
+  user = {"id": user_id, "name": "test", "telefono": "9999-222-21"}
+  # /users/2654?query=query_test
+  # query es un parametro al igual que user_id
+  query= request.args.get('query')
+  if query:
+      user["query"]=query 
+  return jsonify(user), 200
 
 @app.route('/users', methods=['POST'])
+def create_user():
+  data = request.get_json()
+  data["status"] = "user created"
+  return jsonify(data), 201
 
-7:13
+
 if __name__=="__main__":
     app.run(debug=True)
